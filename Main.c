7ec406af9a82aa224478415710785ed7bd0f076f
Rafael Dias .P <foweeytwist@gmail.com>
2026-05-14 10:58:28 -0300

    Update Main.c
    Envio da Atividade 1

--- Main.c
+++ Main.c
@@ -1,1 +1,21 @@
+#include "raylib.h"
 
+int main()
+{
+    InitWindow(800, 600, "Meu Jogo");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(PINK);
+
+        DrawText("Felipe padarias", 20, 10, 40, WHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
