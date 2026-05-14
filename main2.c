6e88cc90970ee0345e51eac2ede927ebd4fa6865
CarlosMarquim <araujocarlorodrigo@gmail.com>
2026-05-14 10:54:47 -0300

    Envio da atividade 1

--- main2.c
+++ main2.c
@@ -1,1 +1,22 @@
+#include "raylib.h"
 
+int main()
+{
+    InitWindow(800, 600, "Meu Primeiro Game");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Bem-Vindo ao Raylib", 10, 10, 40, BLUE) ;
+        DrawText("Pressione ESC para sair", 10, 60, 40, BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
