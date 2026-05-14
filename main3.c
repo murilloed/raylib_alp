ee6462ca3a2dfd929d92c86d0445588c8f67e53d
CarlosMarquim <araujocarlorodrigo@gmail.com>
2026-05-14 11:19:07 -0300

    Envio da atividade 2

--- main3.c
+++ main3.c
@@ -1,1 +1,25 @@
+#include "raylib.h"
 
+int main()
+{
+    InitWindow(800, 450, "basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawRectangle(60, 60, 100, 100, RED);
+
+        DrawRectangle(60, 300, 100, 100, YELLOW);
+        
+        DrawRectangle(400, 60, 200, 100, GREEN);
+        
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
