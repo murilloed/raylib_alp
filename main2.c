4760ff69668ea1c63aba1eb2bd0faa1428f658e7
Gabriel1helberth <gabriel_helberth@outlook.com>
2026-05-14 11:12:50 -0300

    Update main2.c
    Atividade 1

--- main2.c
+++ main2.c
@@ -1,1 +1,23 @@
+#include "raylib.h"
 
+int main()
+{
+    InitWindow(800, 600, "Meu Primeiro Game");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Bem-vindo ao raylib", 10, 10, 40, BLUE);
+        
+        DrawText("Aperte ESC para sair",45,45,35, BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
