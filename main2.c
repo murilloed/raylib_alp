06532f8673b355b3f9031c5a19866c4281eb6668
Felipe-tech30 <felipetorrezanalmeida@gmail.com>
2026-05-14 10:58:18 -0300

    Update main2.c
    Envio da atividade 1

--- main2.c
+++ main2.c
@@ -1,1 +1,22 @@
+#include "raylib.h"
 
+int main()
+{
+    InitWindow(800, 600, "Meu Primeiro Game");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Bem vindo ao Raylib", 50, 10, 40, BLUE);
+        DrawText("Pressione ESC para sair.", 60, 60, 20, RED);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
