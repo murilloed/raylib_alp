a21486fb629a5253ae28bae3fb97afc1b3b8737a
LucasBoani <lucasboani.work@gmail.com>
2026-05-14 11:05:33 -0300

    Update main2.c
    atividade 1

--- main2.c
+++ main2.c
@@ -1,1 +1,21 @@
+#include "raylib.h"
 
+int main()
+{
+    InitWindow(800, 600, "Meu Primeiro Game");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Bem-vindo ao raylib", 10, 10, 40, BLUE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
