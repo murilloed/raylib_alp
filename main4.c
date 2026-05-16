9e2eaa4f1d7fa3b34a15450937209677369b97e1
CarlosMarquim <araujocarlorodrigo@gmail.com>
2026-05-16 15:25:25 -0300

    Envio da atividade 3
    Envio da atividade 3

--- main4.c
+++ main4.c
@@ -1,1 +1,30 @@
+#include "raylib.h"
 
+int main()
+{
+    int posX = 0;
+
+    InitWindow(800, 450, "basic window");
+
+    while (!WindowShouldClose())
+    {
+        posX++;
+
+        if (posX > 800) 
+        {
+            posX = 0;
+        }
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawRectangle(posX, 100, 100, 100, RED);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
