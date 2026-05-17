45a4b7b1fa8b851184681edd91a810cae03172b1
matts006 <matts006@users.noreply.github.com>
2026-05-17 17:59:53 -0300

    Atividade_02

---
+++ main2.c
@@ -0,0 +1,29 @@
+﻿#include "raylib.h"
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "Atividade_02");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawRectangle(100, 100, 100, 100, RED);
+
+        DrawRectangle(300, 150, 220, 120, GREEN);
+
+        DrawRectangle(560, 100, 120, 120, YELLOW);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
